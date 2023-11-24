d742369a56368d2935a0c893e5473bd4d7472155
Snigdha Gupta <snigdhagupta30@gmail.com>
2023-11-24 04:11:41 -0500

    Made code configurable, added readme, and template config.json

---
+++ README.md
@@ -0,0 +1,17 @@
+# Autolabel
+Labelling datasets using evadb's ChatGPT function.
+This app can be used for:
+1. Classification (Binary and multi-class classification)
+2. Named-Entity Recognition.
+
+## Steps:
+1. Create an OpenAI key.
+2. Create a config file which will be used to generate the prompt and label the dataset. 
+3. Run the command given in the Usage section.
+
+## Usage
+python3 autolabel.py --config_file_path 'config.json' --dataset_path './seed2.csv' --result_path './result.csv' --key 'sk-.....'
+
+config_file_path: Path where the config file is stored.
+dataset_path: Path where the dataset is stored. (For now only CSV's are supported).
+result_path: Path where the results will be stored.
--- autolabel.py
+++ autolabel.py
@@ -1,11 +1,36 @@
 import evadb
 import os
 import json
+import argparse
 
 READ_MODE = 'r'
 
-def set_openai_key():
-    os.environ['OPENAI_API_KEY'] = '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'
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file_path',
+                        type=str,
+                        help='Config file path')
+    parser.add_argument(
+        '--dataset_path',
+        type=str,
+        help=
+        'dataset path'
+    )
+    parser.add_argument(
+        '--result_path',
+        type=str,
+        default='./results.csv',
+        help=
+        'file path to save the results'
+    )
+    parser.add_argument('--key',
+                        type=str,
+                        help='OpenAI API key')
+    args = parser.parse_args()
+    return args
+
+def set_openai_key(key):
+    os.environ['OPENAI_API_KEY'] = key
 
 def load_config(file_path):
     with open(file_path, READ_MODE) as file:
@@ -17,48 +42,38 @@ def init_database(config):
     dataset = config['dataset']
     data_column = dataset['data_column']
     label_column = dataset['label_column']
-
-    cursor.query("DROP TABLE IF EXISTS craigslist").df()
-
-    print("table craigslist dropped ")
-
-    cursor.query("CREATE TABLE IF NOT EXISTS craigslist ({} TEXT(10000), {} TEXT(20))".format(data_column, label_column)).df()
-
-    print("Table craigslist created")
+    cursor.query("DROP TABLE IF EXISTS {}".format(config['task_name'])).df()
+    print("Table {} dropped ".format(config['task_name']))
+    cursor.query("CREATE TABLE IF NOT EXISTS {} ({} TEXT(10000), {} TEXT(20))".format(config['task_name'], data_column, label_column)).df()
+    print("Table {} created".format(config['task_name']))
     return cursor
 
-def load_data_into_db(cursor, file_path):
-    cursor.query("LOAD CSV '{}' INTO craigslist".format(file_path)).df()
+def load_data_into_db(cursor, config, file_path):
+    cursor.query("LOAD CSV '{}' INTO {}".format(file_path, config['task_name'])).df()
 
 def label_data(cursor, config, output_file_path):
-
-    # classsification
     prompt = config['prompt']
     dataset = config['dataset']
     labels = prompt['labels']
     data_column = dataset['data_column']
     label_column = dataset['label_column']
     label_str = ''
-
     for label in labels:
         label_str += (label + '\n')
     input_prompt = prompt['task_guidelines'] + " \n " + label_str + " \n " + prompt['output_guidelines']
     context = prompt['example_template'].format(prompt['example_input'], prompt['example_output'])
-
-    query = 'SELECT {}, ChatGPT({}, "{}", "{}") FROM craigslist LIMIT 5'.format(data_column, data_column, context, input_prompt)
+    query = 'SELECT {}, ChatGPT({}, "{}", "{}") FROM {} LIMIT 5'.format(data_column, data_column, context, input_prompt, config['task_name'])
     result_df = cursor.query(query).df()
     result_df.rename(columns={ 'response': label_column }, inplace=True)
-
     result_df.to_csv(output_file_path, index=False)
 
-
-
 def main():
-    set_openai_key()
-    config = load_config('config.json')
+    args = parse_args()
+    set_openai_key(args.key)
+    config = load_config(args.config_file_path)
     cursor = init_database(config)
-    load_data_into_db(cursor, './seed2.csv')
-    label_data(cursor, config, './result2.csv')
+    load_data_into_db(cursor, config, args.dataset_path)
+    label_data(cursor, config, args.result_path)
 
 
 if __name__=="__main__": 
